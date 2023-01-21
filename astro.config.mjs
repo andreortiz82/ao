0c16554bd45ae7ca03526310e024dbda86086c3b
Andre Ortiz <andreortiz82@gmail.com>
2023-01-20 21:51:39 -0600

    tailwind added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()]
+  integrations: [mdx(), sitemap(), react(), tailwind()]
 });
