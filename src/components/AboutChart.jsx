bbbf7cf68a8e9561f2087783d55e1b21cc629aa8
Andre Ortiz <andreortiz82@gmail.com>
2024-02-22 23:08:23 -0600

    updates

--- src/components/AboutChart.jsx
+++ src/components/AboutChart.jsx
@@ -1,30 +1,31 @@
-import style from '../styles/about.module.css'
+import style from "../styles/about.module.css";
 
 export const AboutChart = (props) => {
-
-    return (
-        <div className={style["about-skill-chart"]}>
-            <h5>
-                {props.title}
-            </h5>
-            <div className={style["wrapper"]}>
-                <div className={style["bars"]}>
-                    {props.data.map((skill, i) => {
-                        return (
-                            <div key={skill.label} className={style["bar"]}>
-                                <span className={style["bar-label"]} style={{ width: skill.amount }}>{skill.label}</span>
-                            </div>
-                        )
-                    })}
-                </div>
-                <div className={style["keys"]}>
-                    <span className={style["key"]}>Some Experience</span>
-                    <span className={style["key"]}>Experienced</span>
-                    <span className={style["key"]}>I got this</span>
-                    <span className={style["key"]}>I'll teach you</span>
-                </div>
-            </div>
-
+  return (
+    <div className={style["about-skill-chart"]}>
+      <h5>{props.title}</h5>
+      <div className={style["wrapper"]}>
+        <div className={style["bars"]}>
+          {props.data.map((skill, i) => {
+            return (
+              <div key={skill.label} className={style["bar"]}>
+                <span
+                  className={style["bar-label"]}
+                  style={{ width: skill.amount }}
+                >
+                  {skill.label}
+                </span>
+              </div>
+            );
+          })}
         </div>
-    )
-}
+        <div className={style["keys"]}>
+          <span className={style["key"]}>Some Exposure</span>
+          <span className={style["key"]}>Experienced</span>
+          <span className={style["key"]}>Skills</span>
+          <span className={style["key"]}>Mad Skills</span>
+        </div>
+      </div>
+    </div>
+  );
+};
