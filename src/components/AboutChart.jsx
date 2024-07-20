16533cc58acb4c9eadc36ea5c5a6cade06f7c251
andreortiz82 <andreortiz82@gmail.com>
2024-07-20 10:05:56 -0500

    update

--- src/components/AboutChart.jsx
+++ src/components/AboutChart.jsx
@@ -1,50 +1,14 @@
-import style from "../styles/about.module.css";
-
-export const AboutChart = (props) => {
-  return (
-    <div className={style["about-skill-chart"]}>
-      <h5>{props.title}</h5>
-      <div className={style["wrapper"]}>
-        <div className={style["bars"]}>
-          {props.data.map((skill, i) => {
-            return (
-              <div key={skill.label} className={style["bar"]}>
-                <span
-                  className={style["bar-label"]}
-                  style={{ width: skill.amount }}
-                >
-                  {skill.label}
-                </span>
-              </div>
-            );
-          })}
-        </div>
-        <div className={style["keys"]}>
-          <span className={style["key"]}>Some Exposure</span>
-          <span className={style["key"]}>Experienced</span>
-          <span className={style["key"]}>Skills</span>
-          <span className={style["key"]}>Mad Skills</span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 export const AboutKeywords = (props) => {
   const keywords = props.data
   return (
-    <div className={style["skill-block"]}>
-    <h5>{props.title}</h5>
-    <div className={style["about-skill-keywords"]}>
-      
+    <div className="flex flex-wrap gap-2">
       {keywords.map((keyword, i) => {
         return (
-          <span key={i} className={style["keyword"]}>
+          <span key={i} className="bg-zinc-900 text-zinc-100 dark:bg-zinc-100 dark:text-zinc-900 px-4 py-2 font-semibold rounded-full">
             {keyword.label}
           </span>
         );
       })}
-    </div>
     </div>
   );
 }
--- src/components/ExperienceTimeline.jsx
+++ src/components/ExperienceTimeline.jsx
@@ -1,81 +1,52 @@
-import style from "../styles/about.module.css";
 
-export const ExperienceTimeline = (props) => {
+export const Timeline = (props) => {
+
+  const TimelineItem = (props) => {
+    return (
+      <div className="mb-8 ml-4">
+        <div className="absolute w-3 h-3 bg-zinc-900 dark:bg-zinc-100 rounded-full mt-1.5 -left-1.5 border border-white dark:border-zinc-100"></div>
+        <time className="mb-1 text-md font-normal text-zinc-400 dark:text-zinc-400">{props.date}</time>
+        <h3 className="">{props.title}</h3>
+        <p className="">{props.description}</p>
+      </div>
+    );
+  }
+
   return (
-    <div className={style["about-timline"]}>
-      <h3>Experience</h3>
+    <div className="bg-zinc-50 dark:bg-zinc-800 p-8 rounded">
+      <h2 className="mb-8">Experience</h2>
 
-      <table>
-        <tbody>
-          <tr>
-            <th>2004</th>
-            <td>
-              After college, I started an internship at Sony Red in NYC. By a
-              stroke of luck, one of my personal design projects sparked some
-              interest, and I was invited to collaborate with the design and
-              marketing teams. This fantastic opportunity allowed me to become
-              proficient in Photoshop, Flash, and even pick up some foundational
-              web development skills.
-            </td>
-          </tr>
-          <tr>
-            <th>2006</th>
-            <td>
-              My family and I relocated to Columbus, OH where I gained
+        <div className="relative border-l border-zinc-300 dark:border-zinc-600">
+          <TimelineItem title="Early Sparks" description="I interned for an EDM music label after college. . My educationual focus was sound design and studio recording techniques. While there, I was invited to collaborate with the design and marketing teams. This fantastic opportunity helped me to learn Photoshop, Flash, and even pick up some foundational web design skills." date="2004, NYC New York" />
+          <TimelineItem title="Digging In" description="My family and I relocated to Columbus, OH where I gained
               real-world agency experience. I had the opportunity to design
               materials for prominent clients such as Procter & Gamble,
               Nationwide Insurance, Disney, Toyota USA, and Caterpillar, among
               others. The expectations were high, but I welcomed the challenge.
               I worked diligently to level up in visual design, web development,
-              ActionScript, video production, and motion graphics.
-            </td>
-          </tr>
-          <tr>
-            <th>2009</th>
-            <td>
-              The rise of the iPhone significantly contributed to the decline of
+              ActionScript, video production, and motion graphics." date="2006, Columbus OH" />
+          <TimelineItem title="Adjustments & Growth" description="The rise of the iPhone significantly contributed to the decline of
               Flash. In response, I shifted my focus to HTML, CSS, and
               Javascript. I worked closely with engineers on a daily basis to
-              build products.
-            </td>
-          </tr>
-
-          <tr>
-            <th>2010</th>
-            <td>
-              This period was incredibly exhilarating. Everyone was eager to
+              build products." date="2009, Columbus OH" />
+          <TimelineItem title="Building Foundations" description="This period was incredibly exhilarating. Everyone was eager to
               compete with Facebook, Twitter, and LinkedIn. Using my developed
               skills, I joined small teams aiming to create something innovative
               and fresh. As a designer, my focus shifted more towards product. I
               designed user experiences, keeping business objectives in mind.
-              Although we didn't strike gold, the hustle was life changing.
-            </td>
-          </tr>
-
-          <tr>
-            <th>2012</th>
-            <td>
-              I began frelancing in order to spend more time at home with my
+              Although we didn't strike gold, the hustle was life changing." date="2010, Columbus OH" />
+          <TimelineItem title="Taking Risks" description="I begin freelancing in order to spend more time at home with my
               family. I provided a range of services, including UI/X design,
               branding, design systems, documentation, and front-end
-              development.
-            </td>
-          </tr>
-
-          <tr>
-            <th>2017 - Current</th>
-            <td>
-              After several rewarding years as a freelancer, I chose to rejoin a
+              development." date="2012, Austin TX" />
+          <TimelineItem title="Back in the band" description="After several rewarding years as a freelancer, I chose to rejoin a
               team. I found an exciting opportunity in the healthcare sector and
               was eager to make a significant contribution. Now, I work
               alongside a team of highly skilled designers, developers, and
               product managers to bring medicines to market and improve people's
               lives. My main focus areas are product design, design systems, and
-              relationships.
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              relationships." date="2017 - Current, Austin TX" />
+        </div>
     </div>
   );
 };
