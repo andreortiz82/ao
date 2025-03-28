af9bcc852c1eef789015071d1e72e8910392ce22
Andre Ortiz <andreortiz@Andres-Personal-M4.local>
2025-03-27 23:07:32 -0500

    refine

--- src/components/AboutKeywords.jsx
+++ src/components/AboutKeywords.jsx
@@ -4,7 +4,7 @@ export const Tag = ({ label, size, key }) => {
       <a
         key={key}
         href={`/tag/${label}`}
-        className="bg-zinc-100 text-black px-4 py-1 font-semibold rounded-full text-sm"
+        className="bg-zinc-100 text-black px-4 py-1 font-semibold rounded-full text-sm text-nowrap"
       >
         {label}
       </a>
@@ -14,7 +14,7 @@ export const Tag = ({ label, size, key }) => {
       <a
         key={key}
         href={`/tag/${label}`}
-        className="bg-zinc-100 text-black px-4 py-2 font-semibold rounded-full"
+        className="bg-zinc-100 text-black px-4 py-2 font-semibold rounded-full text-nowrap"
       >
         {label}
       </a>
---
+++ src/content/blog/components.md
@@ -0,0 +1,10 @@
+---
+layout: "../../layouts/BlogPost.astro"
+title: "Components."
+description: "Building components in React."
+pubDate: "2024"
+heroImage: "https://picsum.photos/1920/400"
+tags: ["Tailwind", "Front-end", "React"]
+---
+
+Coming soon...
